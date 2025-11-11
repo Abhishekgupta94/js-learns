fb9acb06a0d12c4bd733b72b11da650e77ad8c22
Abhishek Gupta <maddhesiyaabhishek04@gmail.com>
2025-11-11 05:22:20 +0000

    Done

--- objects.js
+++ objects.js
@@ -18,13 +18,13 @@ delete product.newProperty;
 // console.log(typeof product);
 // console.log(typeof product.name);
 
-/*const Data = {
+const Data = {
     name: 'Raj',
     age: 25,
     ['delivery-time']:'1 Day'//we also add property using Bracket Notation
 };
 
-console.log(Data);
+/*console.log(Data);
 console.log(Data.name);//Dot Notation
 console.log(Data['name']);//Bracket Notation useful when don't work with dor notation 
                           //like console.log(Data.first-name); -->give error
@@ -52,15 +52,53 @@ const product2={
 // product2.fun();
 
 //JSON-->JavaScript Object Notation (Build in js objects with less features)
-console.log(JSON.stringify(product2));//convert product2 from js object to json 
+// console.log(JSON.stringify(product2));//convert product2 from js object to json 
 // console.log(typeof JSON.stringify(product2));//its type is string
 // console.log(product2);//JSON cannot support function inside object
 
 const jsonString = JSON.stringify(product2);
 console.log(JSON.parse(jsonString));//Convert JSON string to JS object... 
-console.log(typeof JSON.parse(jsonString));
+// console.log(typeof JSON.parse(jsonString));
 
 //Another Build in object---> localStorage
 //using local storage we get the value that we saved earlier even we refresh the page...--> Help in Rock-Paper-Scissor
 // Store only string so js object can be convert into string by JSON.stringify();
 
+/* Auto-Boxing--> Java uses auto-boxing its basicalluy a special type of object 
+   that bound the string into object to use methods...
+   exp--> console.log('name'.length) or console.log('name'.toUpperCase()) */
+
+const  obj1={
+    mess:'Hello'
+};
+const obj2=obj1;//object are reference variable so obj1 and obj2 point to same reference
+// console.log(obj1);
+// console.log(obj2);
+const  obj3={
+    mess:'Hello'
+};
+// console.log(obj3===obj1);
+// console.log(obj1===obj2);
+
+const obj4={
+    mess:'Good  Boy!',
+    number:6
+};
+// const mess=obj4.mess;
+const{mess,number}=obj4;//make variable of object property if both have same name
+// const {number}=obj4;---> Destructuring shortcut
+console.log(mess);
+console.log(number);
+
+const obj5={
+    // mess:mess --->Shorthand property if variable and property name are same
+    mess,
+    /*method: function func1(){
+        console.log('Method inside object');
+    } */
+   method(){
+    console.log('Method inside obj5'); //--> called Shorthand Method
+   }
+};
+console.log(obj5);
+obj5.method();
