a6d6573724f2b78b6b8ccd4ada6b12c631c026b6
Abhishek Gupta <maddhesiyaabhishek04@gmail.com>
2025-11-12 07:55:53 +0000

    DOM done

---
+++ dom.js
@@ -0,0 +1,15 @@
+//DOM--->Document Object Model , it rreprresents the webpage(a build0in-object)
+document.body.inertHTML='Hello';//Remove all thing and only display 'Hello'
+
+document.title='Good Boy!';//Change the title to 'Good Boy'!
+console.log(document.bode);//--> Give body element
+
+console.log(typeof document.body);//--->A object
+
+console.log(document.body.innerHTML);//-->Give all html tag inside the html body element
+
+ const bodyElement=document.querySelector('button/.js-class');
+
+ console.log(document.querySelector(button).innerHTML);//-->Give html insidde the button ele.
+
+ 
