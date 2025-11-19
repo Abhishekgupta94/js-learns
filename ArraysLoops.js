f2aad727166d4776730379a64aef88a5123e3d99
Abhishek Gupta <maddhesiyaabhishek04@gmail.com>
2025-11-19 05:24:25 +0000

    Array Done

---
+++ ArraysLoops.js
@@ -0,0 +1,19 @@
+const arr=[10,20,30];
+// console.log(arr);
+// console.log(arr[1]);//Give specific element of arr
+
+arr[0]=80;
+console.log(arr);
+
+const arr1 = [1,'hello',{name:'Abhishek',Roll:8}];
+// console.log(arr1);//we can store number, string and even objects inside an array
+// console.log(typeof arr1); //an object
+
+// console.log(Array.isArray(arr1));//give true
+// console.log(arr1.length);//give size of the array
+
+arr.push(40);//add a value at the last
+console.log(arr);
+
+arr.splice(0,1); // remove value from array take index and nuber of val to remove
+console.log(arr);
